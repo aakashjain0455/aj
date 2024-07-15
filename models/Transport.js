970a8e4e44cd72aa7d2aca10539009f54a48ee58
aakashjain0455 <aakashjain0455@gmail.com>
2024-07-15 17:13:24 +0530

    Add files via upload

---
+++ models/Transport.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Transport = sequelize.define('Transport', {
+  transportCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,  // Define unique constraint here
+  },
+  transportName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  mobile: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  shippingLocation: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Transport;
