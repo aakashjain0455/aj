0f1c4cfb15c7711dbcc6c1e7b6671edda54e4de5
aakashjain0455 <aakashjain0455@gmail.com>
2024-07-15 15:50:00 +0530

    Add files via upload

---
+++ controllers/categoryController.js
@@ -0,0 +1,50 @@
+// controllers/categoryController.js
+const Category = require('../models/Category');
+
+exports.getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch categories' });
+  }
+};
+
+exports.createCategory = async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.json(category);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create category' });
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findByPk(id);
+    if (category) {
+      await category.update(req.body);
+      res.json(category);
+    } else {
+      res.status(404).json({ error: 'Category not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update category' });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findByPk(id);
+    if (category) {
+      await category.destroy();
+      res.sendStatus(204);
+    } else {
+      res.status(404).json({ error: 'Category not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete category' });
+  }
+};
