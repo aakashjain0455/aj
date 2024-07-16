14cab0f53613887cc5143fd4d04cd095d8e56d3b
aakashjain0455 <aakashjain0455@gmail.com>
2024-07-16 13:54:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sequelize = require('./config/db');
+const transportRoutes = require('./routes/transportRoutes'); // Assuming you have this route
+const categoryRoutes = require('./routes/categoryRoutes');
+const subCategoryRoutes = require('./routes/subCategoryRoutes');
+const unitRoutes = require('./routes/unitRoutes'); // Add this line
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Ensure the uploads directory exists
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+app.use('/api', transportRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', subCategoryRoutes);
+app.use('/api', unitRoutes); // Add this line
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Failed to sync database:', err);
+});
