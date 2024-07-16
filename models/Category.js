8a26f4c02275822ea5c4933dff6340341eef9f85
aakashjain0455 <aakashjain0455@gmail.com>
2024-07-16 10:46:28 +0530

    Add files via upload

---
+++ models/Category.js
@@ -0,0 +1,25 @@
+// models/Category.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Category = sequelize.define('Category', {
+  categoryCode: {
+    type: DataTypes.STRING,
+    allowNull: true, // Allow null values for the categoryCode field
+  },
+  categoryName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  photo: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Category;
