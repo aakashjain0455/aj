dc2bf14d616b424b9437aeae00e135e0636c914b
aakashjain0455 <aakashjain0455@gmail.com>
2024-07-17 12:59:34 +0530

    Add files via upload

---
+++ models/MaterialReceipt.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db'); // Ensure this path is correct according to your project structure
+
+const MaterialReceipt = sequelize.define('MaterialReceipt', {
+  voucherNo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  partyName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  remarks: {
+    type: DataTypes.STRING
+  },
+  data: {
+    type: DataTypes.TEXT, // Use TEXT instead of JSON
+    allowNull: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = MaterialReceipt;
