78e8383fafbf4c421b89ad0dc15581c26170b194
aakashjain0455 <aakashjain0455@gmail.com>
2024-07-18 13:06:54 +0530

    Add files via upload

---
+++ controllers/benchmarkController.js
@@ -0,0 +1,42 @@
+const Benchmark = require('../models/Benchmark');
+
+exports.getBenchmarks = async (req, res) => {
+  try {
+    const benchmarks = await Benchmark.findAll();
+    res.json(benchmarks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.createBenchmark = async (req, res) => {
+  try {
+    const { itemCode, benchmark } = req.body;
+    const newBenchmark = await Benchmark.create({ itemCode, benchmark });
+    res.json(newBenchmark);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.updateBenchmark = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { benchmark } = req.body;
+    await Benchmark.update({ benchmark }, { where: { id } });
+    const updatedBenchmark = await Benchmark.findByPk(id);
+    res.json(updatedBenchmark);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deleteBenchmark = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Benchmark.destroy({ where: { id } });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
