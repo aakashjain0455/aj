54fe532da93723c1204b9d99aa946e1168c0da4e
aakashjain0455 <aakashjain0455@gmail.com>
2024-10-29 18:33:09 +0530

    Delete routes/masterSheetRoutes.js

--- routes/masterSheetRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/masterSheetRoutes.js
-const express = require('express');
-const router = express.Router();
-const masterSheetController = require('../controllers/masterSheetController');
-
-router.post('/master-sheets', masterSheetController.createMasterSheet);
-router.get('/master-sheets', masterSheetController.getAllMasterSheets);
-
-module.exports = router;
