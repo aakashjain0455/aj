14e78cb158509c18f8301c05ce17f0e11bc5f76f
aakashjain0455 <aakashjain0455@gmail.com>
2024-12-11 12:37:25 +0530

    Add files via upload

---
+++ routes/punchOrderRoutes.js
@@ -0,0 +1,12 @@
+// routes/punchOrderRoutes.js
+const express = require('express');
+const router = express.Router();
+const punchOrderController = require('../controllers/punchOrderController');
+
+router.get('/punch-orders/next-order-number', punchOrderController.getNextOrderNumber);
+router.post('/punch-orders', punchOrderController.createPunchOrder);
+router.get('/punch-orders', punchOrderController.getAllPunchOrders);
+router.put('/:orderNumber', punchOrderController.updatePunchOrder);
+
+
+module.exports = router;
