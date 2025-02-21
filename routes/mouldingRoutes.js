65c0f5dd60d1b598bb3fc5bca9b42c0d9522db67
aakashjain0455 <aakashjain0455@gmail.com>
2025-02-21 17:09:14 +0530

    Add files via upload

--- routes/mouldingRoutes.js
+++ routes/mouldingRoutes.js
@@ -4,8 +4,6 @@ const mouldingController = require('../controllers/mouldingController');
 
 router.get('/moulding/:orderNumber', mouldingController.getMouldingDataByOrder);
 router.post('/moulding', mouldingController.saveMouldingData);
-router.delete('/moulding/:id', mouldingController.deleteMouldingData);
 router.delete('/moulding/:orderNumber', mouldingController.deleteMouldingByOrder);
-
 
 module.exports = router;
