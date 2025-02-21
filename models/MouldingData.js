896cc184bcae20db4c337592a503bbabb25a8f9c
aakashjain0455 <aakashjain0455@gmail.com>
2025-02-21 11:57:27 +0530

    Add files via upload

---
+++ models/MouldingData.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const MouldingData = sequelize.define('MouldingData', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  orderNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lotNo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tableType: {
+    type: DataTypes.ENUM('Cutting', 'Packing', 'Dispatch'),
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  totalPcs: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  billNo: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
+}, {
+  timestamps: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ['orderNumber', 'lotNo', 'tableType'], // Ensure uniqueness
+    }
+  ]
+});
+
+module.exports = MouldingData;
