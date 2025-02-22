eec6596fc265ffb857d5ef203610b514f0192235
aakashjain0455 <aakashjain0455@gmail.com>
2025-02-22 14:46:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ const MouldingResponse = require('./models/MouldingResponse'); // Import the mod
 const mouldingResponseRoutes = require('./routes/mouldingResponseRoutes');
 const dispatchResponseRoutes = require('./routes/dispatchResponseRoutes');
 const mouldingRoutes = require('./routes/mouldingRoutes');
+const mouldingNewRoutes = require('./routes/mouldingNewRoutes');
 
 
 const app = express();
@@ -94,6 +95,7 @@ app.use('/api', storeResponseRoutes);
 app.use('/api', mouldingResponseRoutes);
 app.use('/api', dispatchResponseRoutes);
 app.use('/api', mouldingRoutes);
+app.use('/api', mouldingNewRoutes);
 
 
 const PORT = process.env.PORT || 5000;
