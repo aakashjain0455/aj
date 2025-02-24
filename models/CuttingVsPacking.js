fd837aec0ad7cf986869fe7f0d32bb6fe1209448
aakashjain0455 <aakashjain0455@gmail.com>
2025-02-24 11:20:35 +0530

    Add files via upload

---
+++ models/CuttingVsPacking.js
@@ -0,0 +1,27 @@
+// CuttingVsPacking.js (Model)
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const CuttingVsPacking = sequelize.define('CuttingVsPacking', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  orderNumber: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lotNo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  data: {
+    type: DataTypes.TEXT, // Stores Cutting, Packing, Dispatch details as JSON
+    allowNull: false
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = CuttingVsPacking;
