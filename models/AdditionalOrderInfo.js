edc8d82512b7dded26c8efd8e5d531e2c2e3a168
aakashjain0455 <aakashjain0455@gmail.com>
2025-03-27 10:59:00 +0530

    Add files via upload

---
+++ models/AdditionalOrderInfo.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const AdditionalOrderInfo = sequelize.define('AdditionalOrderInfo', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  orderNumber: { type: DataTypes.STRING, allowNull: false },
+  expectedDate: { type: DataTypes.STRING },
+  inPlanning: { type: DataTypes.STRING },
+  remarks: { type: DataTypes.TEXT },
+  actuals: { type: DataTypes.TEXT }, // Store actuals as stringified JSON
+}, {
+  timestamps: true
+});
+
+module.exports = AdditionalOrderInfo;
