167b542e5b372da27feda1bef001492721a5f832
aakashjain0455 <aakashjain0455@gmail.com>
2025-04-11 10:27:15 +0530

    Add files via upload

--- models/PunchOrder.js
+++ models/PunchOrder.js
@@ -1,6 +1,5 @@
-// models/PunchOrder.js
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db'); // Replace with your Sequelize instance
+const sequelize = require('../config/db');
 
 const PunchOrder = sequelize.define('PunchOrder', {
   orderNumber: {
@@ -36,10 +35,49 @@ const PunchOrder = sequelize.define('PunchOrder', {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW,
   },
-  userName: {  // ✅ Add this field
+  userName: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+
+  // ✅ Extra Fields (Power Cord + Wire Details)
+  wireSpec: DataTypes.STRING,
+  core: DataTypes.STRING,
+  colour: DataTypes.STRING,
+  length: DataTypes.STRING,
+  brandNameForPlug: DataTypes.STRING,
+  otherBrandName: DataTypes.STRING,
+  clientOfAdvancePayment: DataTypes.STRING,
+  typeOfPin: DataTypes.STRING,
+  backStripping: DataTypes.STRING,
+  backCopperStripping: DataTypes.STRING,
+  terminal: DataTypes.STRING,
+  housing: DataTypes.STRING,
+  sleeve: DataTypes.STRING,
+  solderingTwisting: DataTypes.STRING,
+  backSolder: DataTypes.STRING,
+  packing: DataTypes.STRING,
+  grommetName: DataTypes.STRING,
+  grommetLength: DataTypes.STRING,
+  plug: DataTypes.STRING,
+  computerSocket: DataTypes.STRING,
+  amp: DataTypes.STRING,
+  remarksPowerCord: DataTypes.STRING,
+
+  noOfStands: DataTypes.STRING,
+  copperStandsDia: DataTypes.STRING,
+  brandNameOnWire: DataTypes.STRING,
+  coreColours: DataTypes.STRING,
+  coreOd: DataTypes.STRING,
+  corePVC: DataTypes.STRING,
+  outerOd: DataTypes.STRING,
+  pvcInOuter: DataTypes.STRING,
+  finishing: DataTypes.STRING,
+  printingOnWire: DataTypes.STRING,
+  wireCmlNo: DataTypes.STRING,
+  planCoilWeight: DataTypes.STRING,
+  coilLength: DataTypes.STRING,
+  remarksWire: DataTypes.STRING,
 });
 
 module.exports = PunchOrder;
