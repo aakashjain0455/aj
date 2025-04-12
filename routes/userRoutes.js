78c9a28737bf6f149ff1adefe03dc70187676673
aakashjain0455 <aakashjain0455@gmail.com>
2025-04-12 14:41:22 +0530

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,7 @@ router.delete('/users/:id', userController.deleteUser);
 router.post('/users/signin', userController.signIn);
 router.post('/users/change-password', userController.changePassword);  // New route
 
+router.post('/users/logoutAll', userController.logoutAllUsers);
+router.get('/users/sessionVersion', userController.getSessionVersion);
+
 module.exports = router;
