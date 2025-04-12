ee25adc091563ede538ba3b0d4d0217fa269c311
aakashjain0455 <aakashjain0455@gmail.com>
2025-04-12 10:34:48 +0530

    Update punchOrderRoutes.js

--- routes/punchOrderRoutes.js
+++ routes/punchOrderRoutes.js
@@ -6,5 +6,6 @@ const punchOrderController = require('../controllers/punchOrderController');
 router.get('/punch-orders/next-order-number', punchOrderController.getNextOrderNumber);
 router.post('/punch-orders', punchOrderController.createPunchOrder);
 router.get('/punch-orders', punchOrderController.getAllPunchOrders);
+router.put('/punch-orders/by-master/:masterId', punchOrderController.updatePunchOrdersByMasterId);
 
 module.exports = router;
